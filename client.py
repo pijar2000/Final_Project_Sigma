64654ddc0a394c11f9322416c74bbe51d65a6462
pijar2000 <pijar2000@gmail.com>
2024-05-26 00:54:01 +0700

    Testing to data client

--- client.py
+++ client.py
@@ -1,31 +1,21 @@
-import csv
+import pandas as pd
 import json
 import requests
 
-data = [[4, 177, 3, 0, 44, 1233],
-        [4, 273, 3, 1, 2, 4374],
-        [5, 180, 2, 0, 40, 1240],
-        [4, 200, 5, 1, 50, 1200],
-        [5, 245, 3, 0, 9, 4694],
-        [6, 190, 4, 1, 48, 1250],
-        [2, 150, 2, 0, 38, 1210],
-        [4, 270, 3, 0, 1, 4131],
-        [5, 167, 2, 1, 2, 5908],
-        [3, 170, 2, 1, 43, 3245]]
+data = pd.read_csv('data_client.csv')
+
+# Convert DataFrame to list
+data_list = data.values.tolist()
 
 url = 'http://localhost:8000/predict/'
 
 predictions = []
-for record in data:
+for record in data_list:
     payload = {'features': record}
     response = requests.post(url, json=payload)
+    response.raise_for_status() 
     prediction = response.json()['prediction']
     predictions.append(prediction)
 
-# save to CSV
-with open('prediction_web.csv', 'w', newline='') as csvfile:
-    csvwriter = csv.writer(csvfile)
-    csvwriter.writerow(['prediction'])
-    csvwriter.writerows([[pred] for pred in predictions])
-
-print(predictions)
+# Save to CSV
+pd.DataFrame(predictions, columns=['Prediction']).to_csv('prediction_from_web.csv')
