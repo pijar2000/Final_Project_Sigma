5a9902d89fbc5b07b80aa3ea76b2238c8606adcb
pijar2000 <pijar2000@gmail.com>
2024-05-26 00:09:02 +0700

    Penambahan struktur pipeline untuk deployment ML

---
+++ app/server.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
+
+model, optimal_thresh = joblib.load('app/model_pandas.joblib')
+
+class_names = np.array(['0', '1'])  # Assuming '0' is for on-time and '1' is for late
+
+app = FastAPI()
+
+@app.get('/')
+def read_root():
+    return {'message': 'Shipping Model API'}
+
+@app.post('/predict')
+def predict(data: dict):
+    # Extract features from the request
+    features = np.array(data['features'])
+
+    # Perform prediction
+    prediction = model.predict(features.reshape(1, -1))
+    predicted_class = class_names[prediction][0]
+
+    return {'prediction': predicted_class}
---
+++ client.py
@@ -0,0 +1,31 @@
+import csv
+import json
+import requests
+
+data = [[4, 177, 3, 0, 44, 1233],
+        [4, 273, 3, 1, 2, 4374],
+        [5, 180, 2, 0, 40, 1240],
+        [4, 200, 5, 1, 50, 1200],
+        [5, 245, 3, 0, 9, 4694],
+        [6, 190, 4, 1, 48, 1250],
+        [2, 150, 2, 0, 38, 1210],
+        [4, 270, 3, 0, 1, 4131],
+        [5, 167, 2, 1, 2, 5908],
+        [3, 170, 2, 1, 43, 3245]]
+
+url = 'http://localhost:8000/predict/'
+
+predictions = []
+for record in data:
+    payload = {'features': record}
+    response = requests.post(url, json=payload)
+    prediction = response.json()['prediction']
+    predictions.append(prediction)
+
+# Menyimpan hasil prediksi ke dalam file CSV
+with open('prediction_web.csv', 'w', newline='') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(['prediction'])
+    csvwriter.writerows([[pred] for pred in predictions])
+
+print(predictions)
